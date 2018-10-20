f519883ee4f1fdbd772935dd513c62659279315a
ASisouphone <sisouphone.alan@gmail.com>
2018-10-20 09:24:21 -0400

    new client

--- sdks/java/AI/src/com/atomicobject/othello/AI.java
+++ sdks/java/AI/src/com/atomicobject/othello/AI.java
@@ -3,12 +3,37 @@ package com.atomicobject.othello;
 import java.util.Arrays;
 import java.util.ListIterator;
 
+import java.util.Stack;
+import java.util.ArrayList;
+
 public class AI {
-	
+	class Node() {
+		public int c;
+		public int r;
+		public int[] d;
+
+		public ArrayList<Node> children;
+
+		public Node(int c, int r, int[] d) {
+			this.c = c;
+			this.r = r;
+			this.d = d;
+		}
+	}
 	ListIterator<int[]> moveList;
+	
+	private int depth;
+
+	private int color;
+
+	private int enemyColor;
+
+	private Stack<int[][]> stateStack;
 
 	public AI(int[][] moves) {
-		moveList = Arrays.asList(moves).listIterator();
+		stateStack = new Stack<int[][]>();
+		depth = 3; // CHANGE DEPTH 
+		// /moveList = Arrays.asList(moves).listIterator();
 	}
 
 	public int[] computeMove(GameState state) {
@@ -16,5 +41,66 @@ public class AI {
 		return moveList.next();
 	}
 
-	
+	private static int[][] cloneArray(int[][] src) {
+	    int length = src.length;
+	    int[][] target = new int[length][src[0].length];
+	    for (int i = 0; i < length; i++) {
+	        System.arraycopy(src[i], 0, target[i], 0, src[i].length);
+	    }
+	    return target;
+	}
+
+	private boolean isGameOver() {
+		return false;
+	}
+
+	private ArrayList<Node> getChildrenNodes(boolean isMax) {
+		int color, enemyColor;
+		if (isMax) {
+			color = this.color;
+			enemyColor = this.enemyColor;
+		} else {
+			color = this.enemyColor;
+			enemyColor = this.color;
+		}
+		int[][] board = stateStack.peek();
+		ArrayList<int[]> potentialMoves = new ArrayList<int[]>();
+
+		for (int i = 0; i < 8; i++)
+			for (int j = 0; j < 8; j++) {
+				if (board[i][j] == enemyColor)
+			}
+
+	}
+
+	private void doMove(int c, int r, int[] d) {
+
+	}
+
+	private void undoMove() {
+		stateStack.pop();
+	}
+
+	private int negamax(Node node, int depth, int alpha, int beta) {
+		if (depth == 0 || isGameOver()) {
+			// evaluateState() relative to current player
+		}
+		boolean isMax = (stateStack.size() % 2 == 1);
+		node.children = getChildrenNodes(isMax);
+		int eval = -8000;
+		for (int i = 0; i < node.children.size(); i++) {
+			Node child = node.children.get(i);
+			doMove(child.c, child.r, child.d);
+			eval = negamax(child, depth - 1, -beta, -alpha);
+			undoMove();
+			alpha = Integer.max(alpha, eval);
+			if (depth == this.depth) {
+				if (alpha == eval) {
+					// create bestMove here!
+				}
+			}
+		}
+
+
+	}
 }
--- sdks/java/AI/src/com/atomicobject/othello/Client.java
+++ sdks/java/AI/src/com/atomicobject/othello/Client.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 import com.google.gson.Gson;
 
@@ -39,6 +40,29 @@ public class Client {
 		closeStreams();
 	}
 
+	// Start client with user input
+	public void startWithInput() {
+		System.out.println("Starting client with input ...");
+		GameState state;
+		try {
+			while ((state = readStateFromServer()) != null) {
+				int[] inputMove = new int[2];
+				System.out.println("Board:\n" + state.toString());
+				Scanner reader = new Scanner(System.in);  // Reading from System.in
+				System.out.println("Enter a move: ");
+				inputMove[0] = reader.nextInt(); // Scans the next token of the input as an int.
+				inputMove[1] = reader.nextInt();
+				int[] move = ai.computeMove(state);
+				respondWithMove(move);
+				reader.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		closeStreams();
+	}
+	
 	private GameState readStateFromServer() throws IOException {
 		System.out.println("Reading from server ...");
 		String nextLine = input.readLine();
